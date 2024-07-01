d0d94acbb69f5f20e24f2c052b517506b571e33e
Majuri95 <majuriinte2005@gmail.com>
2024-06-30 20:20:13 -0500

    Add files via upload

---
+++ Python/operadores.py
@@ -0,0 +1,42 @@
+"""Operadores en Python
+operadores aritméticos"""
+a=2
+b=3
+suma=a+b#esto es una resta
+print("la suma de:", a, "más", b, "es:", suma)
+resta=a-b#esto es una resta
+print("la resta de:", a, "menos", b, "es:", resta)
+multiplicación=a*b#esto es una multiplicación
+print("la multiplicación de:", a, "multiplicado", b, "es:", multiplicación)
+división=a/b#esto es una división
+print("la división de:", a, "dividido", b, "es:", división)
+potenciación=a**b#es una potenciación
+print("la potenciación de:", a, "elevado", b, "es:", potenciación)
+
+#Operadores Relacionales
+a=6
+b=7
+c=2
+d=1
+print(a, "es mayor que", b, a > b)
+print(b, "es mayor que", d, b > c)
+print(d, "es menor o igual", c, d < c)
+print(c, "mayor o igual", a, c > a)
+
+"""Operadores Lógicos"""
+p = 5 > 2
+q = 2 < 1
+p or q
+p and q
+(not p or q)
+print("La proposición p o q es:", p or q)
+print("La proposición p y q es:", p and q)
+print("La proposición not p or q:", not p or q)
+
+#Tipos de divisiones
+"""Entera"""
+27//14
+"""Decimal"""
+27/14
+"""Conversión de expresiones"""
+
---
+++ Python/primerprogama.py
@@ -0,0 +1,6 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hola mundo soy Majuri Inte de Economía E1 - 004, Estadística")
+Hola mundo soy Majuri Inte de Economía E1 - 004, Estadística
+>>> print("Hasta mañana mundo lindo")
+Hasta mañana mundo lindo
